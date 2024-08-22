bc3e5383405eb60c77f2609c3ed68cff4906449a
Devin Ulrey <Devin.Ulrey@gmail.com>
2024-08-22 07:06:45 -0400

    - Ceil the (de)buff duration to make sure it lasts long enough on the client

--- World/Data/Scripts/System/Misc/BuffIcons.cs
+++ World/Data/Scripts/System/Misc/BuffIcons.cs
@@ -400,7 +400,8 @@ namespace Server
 			if( length < TimeSpan.Zero )
 				length = TimeSpan.Zero;
 
-			m_Stream.Write( (short)length.TotalSeconds );	//Time in seconds
+			// Keep the (de)buff applied longer rather than shorter
+			m_Stream.Write( (short)Math.Ceiling(length.TotalSeconds) );	//Time in seconds
 
 			m_Stream.Fill( 3 );
 			m_Stream.Write( (int)titleCliloc );
